b2942cf9098a334f50f168ddb66c946a2d9a09d8
joaoperfig <joaoperfig@gmail.com>
2019-01-18 15:10:42 +0100

    make storemap singleton

--- src/main/java/sa/StoreMap.java
+++ src/main/java/sa/StoreMap.java
@@ -3,9 +3,13 @@ import java.util.ArrayList;
 
 public class StoreMap {
 	
+	public static StoreMap instance = null; //singleton
+	
 	public float lenx; //map width
 	public float leny; //map height
 	// Objects can be in positions ([0, lenx], [0, leny])
+	public int storesNumber;
+	public int peopleNumber;
 	
 	public ArrayList<Customer> customers; //Customers in this map
 	public ArrayList<Store> stores; //Stores in this map
@@ -13,6 +17,13 @@ public class StoreMap {
 	public StoreMap(float width, float height, int peopleNumber, int productsNumber, int storesNumber) {
 		// CREATE LIST OF COSTUMERS IN RANDOM POSITIONS (positions go from 0 to width and 0 to height)
 		// CREATE LIST OF STORES WITH RANDOM PRODUCTS AND RANDOM PRICES
+	}
+	
+	public static StoreMap getInstance() {
+		if (instance == null) {
+			instance = new StoreMap(40, 40, 40, 4, 4);
+		}
+		return instance;
 	}
 	
 	public float getTotalUnhappiness() {
---
+++ src/main/java/sa/StoresPhenotype.java
@@ -0,0 +1,25 @@
+package sa;
+
+public class StoresPhenotype extends Phenotype {
+	
+	public StoresPhenotype() { //Construct Phenotype from random parameters (for initial population)
+		
+	}
+	
+	public StoresPhenotype(Genotype genotype) { //Decode Genotype and construct Phenotype from it
+		
+	}
+
+	@Override
+	public Genotype encode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public float evaluate() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
